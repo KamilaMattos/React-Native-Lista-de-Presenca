595305eba6096977f02107016b7abb72a331ee51
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-05-27 18:23:13 -0300

    feature: using useState to capture names from input and render on list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import moment from "moment"
 require("moment/locale/pt-br")
-
+import React, { useState } from "react"
 import {
   Text,
   View,
@@ -9,34 +9,27 @@ import {
   FlatList,
   Alert,
 } from "react-native"
+
 import { styles } from "./styles"
 
 import { Participant } from "../../components/Participant"
 
 export function Home() {
-  const participants = [
-    "Kamila",
-    "Logan",
-    "Eduardo",
-    "Yoshi",
-    "Joao",
-    "Dani",
-    "Claudia",
-    "Thiago",
-    "Davi",
-    "Juninho",
-  ]
+  const [participants, setParticipants] = useState<string[]>([])
+  const [participantName, setParticipantName] = useState("")
 
   const dateNow = moment()
   const formatDate = dateNow.format("dddd, DD [de] MMMM [de] YYYY")
 
   function handleAddParticipant() {
-    if (participants.includes("Kamila")) {
+    if (participants.includes(participantName)) {
       return Alert.alert(
         "Participante presente!",
         "Este participante já está presente no evento"
       )
     }
+    setParticipants((prevState) => [...prevState, participantName])
+    setParticipantName("")
   }
 
   function handleRemoveParticipant(name: string) {
@@ -66,6 +59,8 @@ export function Home() {
           style={styles.input}
           placeholder='Nome do partcipante'
           placeholderTextColor='#6b6b6b'
+          onChangeText={(text) => setParticipantName(text)}
+          value={participantName}
         />
 
         <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
