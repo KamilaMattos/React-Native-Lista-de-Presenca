446d82d489b5ce9081d5efcdd76aad5a0d8039c6
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-05-27 16:14:46 -0300

    feature: adding input text

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
 import moment from "moment"
 require("moment/locale/pt-br")
 
-import { Text, View } from "react-native"
+import { Text, View, TextInput } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
@@ -12,6 +12,11 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>{formatDate}</Text>
+      <TextInput
+        style={styles.input}
+        placeholder='Nome do partcipante'
+        placeholderTextColor='#6b6b6b'
+      />
     </View>
   )
 }
