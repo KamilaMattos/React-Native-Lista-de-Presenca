1a98df95cadf72bce96f794037b857d475ede1b1
Kamila Mattos <99353050+KamilaMattos@users.noreply.github.com>
2023-05-27 16:26:37 -0300

    feature: added add participant button

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,31 @@
 import moment from "moment"
 require("moment/locale/pt-br")
 
-import { Text, View, TextInput } from "react-native"
+import { Text, View, TextInput, TouchableOpacity } from "react-native"
 import { styles } from "./styles"
 
 export function Home() {
   const dateNow = moment()
   const formatDate = dateNow.format("dddd, DD [de] MMMM [de] YYYY")
 
+  function handleAddParticipant() {
+    console.log("Add new participant")
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>{formatDate}</Text>
+
       <TextInput
         style={styles.input}
         placeholder='Nome do partcipante'
         placeholderTextColor='#6b6b6b'
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   )
 }
